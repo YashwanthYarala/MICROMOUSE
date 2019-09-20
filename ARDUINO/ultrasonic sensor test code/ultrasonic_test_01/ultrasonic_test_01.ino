58862098ce43100d973e483f8c9f66c93b2eaa28
Yashwanth Reddy <yarala.yashwanthreddy1905@gmail.com>
2019-09-20 19:47:12 +0530

    this is regarding creating directories and making test files to check github conectivity
    Arduino folder is added

---
+++ ARDUINO/ultrasonic sensor test code/ultrasonic_test_01/ultrasonic_test_01.ino
@@ -0,0 +1,22 @@
+ int trigpin = 34;
+     int echopin = 30;
+void setup() {
+  // put your setup code here, to run once:
+    
+     Serial.begin(9600);
+     pinMode(trigpin,OUTPUT);
+     pinMode(echopin,INPUT);
+     
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   long duration , distance ;
+   digitalWrite(trigpin,HIGH);
+   delayMicroseconds(1000);
+   digitalWrite(trigpin,LOW);
+   duration = pulseIn(echopin,HIGH);
+   distance = (duration/2)/29.1;
+   Serial.println(distance);
+   delay(10);
+}
