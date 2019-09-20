43e3ce64066e126a5e6fdfd805c812e411e689d7
Yashwanth Reddy <yarala.yashwanthreddy1905@gmail.com>
2019-09-21 00:56:38 +0530

    basics of python being practiced

---
+++ CODES/Python_Basics/1d array.py
@@ -0,0 +1,26 @@
+# for loop
+friends = ['yashwnth','reddy','yarala']
+for x in friends:
+    print(x)
+    print(len(x))  #for each x it gives no.of elements in the x
+for x in friends[:4]:
+    print(x)
+sum = 0
+for x in range(10): #this range includes numbers from 0 t0 9
+    sum +=x
+    print(sum)
+#functions
+def func1(a,b):
+    print(a+b)
+func1(2,3)
+#sets
+marks = {"12","13","15","18","21","32","12"}  #it doesnt consider about repititions
+print(marks)
+#dictionary
+#here we use keys and values instead of word and meaning respectively
+
+
+
+
+
+
