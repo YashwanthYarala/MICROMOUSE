d9769bdd9ac16ff08afc69084069ad8bbfb02503
Yashwanth Reddy <yarala.yashwanthreddy1905@gmail.com>
2019-10-22 23:42:14 +0530

    matlab directory created

--- CODES/2d_array.py
+++ CODES/2d_array.py
@@ -9,5 +9,4 @@ maze_potential =[[6,5,4,3,3,4,5,6],
                  [5,4,3,2,2,3,4,5],
                  [6,5,4,3,3,4,5,6]]
 maze_path = maze_potential
-for i in range(8):
-    for j in range(8):
+
