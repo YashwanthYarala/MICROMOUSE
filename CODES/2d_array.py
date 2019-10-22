5b807dc6251eae992bd95af908b7c98a9156642a
Yashwanth Reddy <yarala.yashwanthreddy1905@gmail.com>
2019-10-23 00:11:16 +0530

    matlab directory created

--- CODES/2d_array.py
+++ CODES/2d_array.py
@@ -9,4 +9,19 @@ maze_potential =[[6,5,4,3,3,4,5,6],
                  [5,4,3,2,2,3,4,5],
                  [6,5,4,3,3,4,5,6]]
 maze_path = maze_potential
+for i in range(8):
+    for j in range(8):
+        a = maze_path[i][j]
+        if(i!=7):
+            b = maze_path[i+1][j]
+            if(a>b):
+                maze_potential[i+1][j]=1
+
+            print(b)
+        elif (i==7):
+            b = maze_path[i][j]
+            print(b)
+
+
+
 
