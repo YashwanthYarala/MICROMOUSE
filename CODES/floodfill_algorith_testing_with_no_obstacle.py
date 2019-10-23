bf7d3c97cd74bd984cac073ddc9aa504820d089d
Yashwanth Reddy <yarala.yashwanthreddy1905@gmail.com>
2019-10-23 19:24:19 +0530

    flood fill in progress

--- CODES/floodfill_algorith_testing_with_no_obstacle.py
+++ CODES/floodfill_algorith_testing_with_no_obstacle.py
@@ -21,18 +21,17 @@ p_y = 0
 #starting from first element
 #p_x and p_y be the positions
 def flood_fill():
+    p_y = 0
     for p_x in range(7):
         if(maze_path[p_x][p_y]>maze_path[p_x+1][p_y]):
             #if yes we move down
             maze_potential[p_x+1][p_y]=1
         elif(maze_path[p_x][p_y]>maze_path[p_x][p_y+1]):
             maze_potential[p_x][p_y+1] = 1
+            p_x = p_x-1
+            p_y=p_y+1
 
     print(maze_potential)
-
-
-
-
 
 
 flood_fill()
