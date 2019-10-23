be34e302ba08feaf338402b7683f6bfe32c14c62
Yashwanth Reddy <yarala.yashwanthreddy1905@gmail.com>
2019-10-23 18:58:55 +0530

    flood fill in progress

--- CODES/floodfill_algorith_testing_with_no_obstacle.py
+++ CODES/floodfill_algorith_testing_with_no_obstacle.py
@@ -8,11 +8,34 @@ maze_potential =[[6,5,4,3,3,4,5,6],
                  [4,3,2,1,1,2,3,4],
                  [5,4,3,2,2,3,4,5],
                  [6,5,4,3,3,4,5,6]]
-maze_path = maze_potential
+maze_path = [[6,5,4,3,3,4,5,6],
+                 [5,4,3,2,2,3,4,5],
+                 [4,3,2,1,1,2,3,4],
+                 [3,2,1,0,0,1,2,3],
+                 [3,2,1,0,0,1,2,3],
+                 [4,3,2,1,1,2,3,4],
+                 [5,4,3,2,2,3,4,5],
+                 [6,5,4,3,3,4,5,6]]
+p_x = 0
+p_y = 0
 #starting from first element
 #p_x and p_y be the positions
+def flood_fill():
+    for p_x in range(7):
+        if(maze_path[p_x][p_y]>maze_path[p_x+1][p_y]):
+            #if yes we move down
+            maze_potential[p_x+1][p_y]=1
+        elif(maze_path[p_x][p_y]>maze_path[p_x][p_y+1]):
+            maze_potential[p_x][p_y+1] = 1
+
+    print(maze_potential)
 
 
+
+
+
+
+flood_fill()
 
 
 
