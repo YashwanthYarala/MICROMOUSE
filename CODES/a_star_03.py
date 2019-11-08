16844df8f24106c3d87a20f57f7c6f4f27bd9cc8
Yashwanth Reddy <yarala.yashwanthreddy1905@gmail.com>
2019-11-08 12:58:01 +0530

    a_star_03  :  going in diagonal direction

--- CODES/a_star_03.py
+++ CODES/a_star_03.py
@@ -36,6 +36,33 @@ while(a!=4 and b!=4):
     else:
         t_u = cost(a,b)
 # comparison between costs
+    if(t_d>t_l or t_d>t_r or t_d>t_u):
+        a =a
+        b= b
+        #then t_d is not the least values of cost
+    else:
+        # then t_d is the least value
+        b = b + 1
+    if (t_r > t_l or t_r > t_u or t_r > t_d):
+        a = a
+        b = b
+    else:
+        a += 1
+    if (t_l > t_u or t_l > t_r or t_l > t_d):
+        a = a
+        b = b
+    else:
+        a = a - 1
+
+    if (t_u > t_l or t_u > t_r or t_r > t_d):
+        a = a
+        b = b
+    else:
+        b = b - 1
+if(a==4 and b==4):
+    maze[a][b]="*"
+
+print(maze)
 
 
 
