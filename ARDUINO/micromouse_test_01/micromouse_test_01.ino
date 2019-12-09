75e4884d6099da0de67f22ee883761aa41f14caf
Yashwanth Reddy <yarala.yashwanthreddy1905@gmail.com>
2019-12-09 19:34:31 +0530

    a_star_03  :  going in diagonal direction

---
+++ ARDUINO/micromouse_test_01/micromouse_test_01.ino
@@ -0,0 +1,37 @@
+ int trigpin = 10;
+     int echopin = 9;
+void setup() {
+  // put your setup code here, to run once:
+    
+     Serial.begin(9600);
+     pinMode(trigpin,OUTPUT);
+     pinMode(echopin,INPUT);
+     //declaration of pins is done 
+     
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   long duration , distance ;
+   digitalWrite(trigpin,HIGH);
+   delayMicroseconds(1000);
+   digitalWrite(trigpin,LOW);
+   duration = pulseIn(echopin,HIGH);
+   distance = (duration/2)/29.1;
+   Serial.println(distance);  //prints the distnce
+   //delay(10);
+   if (distance < 8)
+   {
+      digitalWrite(3,LOW);
+      digitalWrite(11,HIGH);
+      digitalWrite(13,HIGH);
+   }
+   else
+   {
+      digitalWrite(3,HIGH);
+      digitalWrite(11,HIGH);
+      digitalWrite(13,HIGH);
+      digitalWrite(12,HIGH);
+   }
+   
+}
